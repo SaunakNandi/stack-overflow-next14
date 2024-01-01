71da253fef27ebe1f07f89fcc370fca2854c76c7
Saunak Nandi <saunaknandi021000@gmail.com>
2024-01-01 20:14:21 +0530

    Making changes for the error

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -9,50 +9,98 @@ import Question from "@/database/question.model"
 import { connectToDatabase } from "../mongoose"
 import Tag from "@/database/tag.model"
 
+// export async function createQuestion(params: any) {
+//     try {
+//         // connect to DB
+//         connectToDatabase()
+//         // path is the url to the page we have reloaded, here Home page. Its so because we have to re-validate the page
+//         // so that nextjs knows something has changed
+//         const {title,content,tags,author,path}=params
+        
+//         // create the question
+//         const question = await Question.create({
+//             title,
+//             content,
+//             author
+//         })
+
+//         const tagDocuments=[]
+
+//         // Create the tag or get them if they already exist
+
+//         for(const tag of tags)
+//         {
+//             //console.log(tag)
+//             // check the NOTE.txt
+
+//     // findOneAndUpdate: A MongoDB method used to find a document based on specified criteria and update it,
+//     //  or insert a new document if the criteria do not match any existing documents.
+
+//             const existingTag=await Tag.findOneAndUpdate(
+//                 { name: { $regex: new RegExp(`^${tag}$`, "i") } }, 
+//                 { $setOnInsert: { name: tag }, $push: { question: question._id } },
+//                 { upsert: true, new: true }
+//             )
+//             tagDocuments.push(existingTag._id)
+//         }
+//         //console.log(tagDocuments)
+//         
+//         await Question.findByIdAndUpdate(question._id,{
+//             $push: { tags: { $each: tagDocuments }}  // for each tagDocument we push the id of that tag
+//         })
+
+//         // Create an interaction record for the user's ask_question action. This is to tag which user created this perticular question
+        
+//         // Increment author's reputation by +5 for creating a question
+//     } catch (error) {
+        
+//     }
+// }
+
 export async function createQuestion(params: any) {
     try {
         // connect to DB
-        connectToDatabase()
-        // path is the url to the page we have reloaded, here Home page. Its so because we have to re-validate the page
+      connectToDatabase();
+    // path is the url to the page we have reloaded, here Home page. Its so because we have to re-validate the page
         // so that nextjs knows something has changed
-        const {title,content,tags,author,path}=params
+      const { title, content, tags, author, path } = params;
+  
+      // Create the question
+      const question = await Question.create({
+        title,
+        content,
+        author
+      });
+  
+      const tagDocuments = [];
+  
+      // Create the tags or get them if they already exist
+      for (const tag of tags) {
+
         
-        // create the question
-        const question = await Question.create({
-            title,
-            content,
-            author
-        })
-
-        const tagDocuments=[]
-
-        // Create the tag or get them if they already exist
-
-        for(const tag of tags)
-        {
-            //console.log(tag)
-            // check the NOTE.txt
-
-    // findOneAndUpdate: A MongoDB method used to find a document based on specified criteria and update it,
-    //  or insert a new document if the criteria do not match any existing documents.
-
-            const existingTag=await Tag.findOneAndUpdate(
-                { name: { $regex: new RegExp(`^${tag}$`, "i") } }, 
-                { $setOnInsert: { name: tag }, $push: { question: question._id } },
-                { upsert: true, new: true }
-            )
-            tagDocuments.push(existingTag._id)
-        }
-        //console.log(tagDocuments)
-        // find the we question created and update
-        await Question.findByIdAndUpdate(question._id,{
-            $push: { tags: { $each: tagDocuments }}  // for each tagDocument we push the id of that tag
-        })
-
-        // Create an interaction record for the user's ask_question action. This is to tag which user created this perticular question
+// findOneAndUpdate: A MongoDB method used to find a document based on specified criteria and update it,
+//  or insert a new document if the criteria do not match any existing documents.
+        const existingTag = await Tag.findOneAndUpdate(
+          { name: { $regex: new RegExp(`^${tag}$`, "i") } }, 
+          { $setOnInsert: { name: tag }, $push: { questions: question._id } },
+          { upsert: true, new: true }
+        )
+  
+        tagDocuments.push(existingTag._id);
+      }
+  // find the question we created and push the tags
+      await Question.findByIdAndUpdate(question._id, {
+        $push: { tags: { $each: tagDocuments }}       // for each tagDocument we push the id of that tag
+      });
+  
+    // Create an interaction record for the user's ask_question action. This is to tag which user created this perticular question
         
-        // Increment author's reputation by +5 for creating a question
-    } catch (error) {
-        
+       // Increment author's reputation by +5 for creating a question    
+    } 
+    catch (error) {
+      console.log(error);
     }
-}
+  }
+  
+ 
+  
