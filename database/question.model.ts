0dd7018072516a5b6e1842ae2546bb2e6d24a8aa
Saunak Nandi <saunaknandi021000@gmail.com>
2024-01-02 08:34:56 +0530

    small bug

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,32 +1,32 @@
-import { Schema,model,models, Document } from "mongoose"
+import { Schema, model, models, Document } from "mongoose";
 
 // Creating a Question Model using Mongoose
-export interface IQuestion extends Document{
-    title:string;
-    content:string;
-    tags:Schema.Types.ObjectId[];
-    views:number;
-    upvotes:Schema.Types.ObjectId[];  // connecting to another model in the database
-    downvotes:Schema.Types.ObjectId[];   // array of ObjectIds of people that have voted that post 
-    author:Schema.Types.ObjectId;
-    answers:Schema.Types.ObjectId[];
-    createdAt:Date;
+export interface IQuestion extends Document {
+  title: string;
+  content: string;
+  tags: Schema.Types.ObjectId[];
+  views: number;
+  upvotes: Schema.Types.ObjectId[]; // connecting to another model in the database
+  downvotes: Schema.Types.ObjectId[]; // array of ObjectIds of people that have voted that post
+  author: Schema.Types.ObjectId;
+  answers: Schema.Types.ObjectId[];
+  createdAt: Date;
 }
 
-const QuestionSchema=new Schema({
-    title: { type: String, required: true },
-    explanation: { type: String, required: true },
-    tags: [{type:Schema.Types.ObjectId,ref:"Tag"}],
-    views:{type:Number,default:0},
-    upvotes:[{type:Schema.Types.ObjectId,ref:"User"}],
-    downvotes:[{type:Schema.Types.ObjectId,ref:"User"}],
-    author:[{type:Schema.Types.ObjectId,ref:"User"}],
-    answers:[{type:Schema.Types.ObjectId,ref:"Answer"}],
-    createdAt:{type:Date,default:Date.now()},
-})
+const QuestionSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  views: { type: Number, default: 0 },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  author: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  answers: [{ type: Schema.Types.ObjectId, ref: "Answer" }],
+  createdAt: { type: Date, default: Date.now() },
+});
 
 // make model from QuestionSchema
 // check if model is present or not if not them create a model
-const Question=models.Question || model('Question',QuestionSchema)
+const Question = models.Question || model("Question", QuestionSchema);
 
-export default Question
+export default Question;
