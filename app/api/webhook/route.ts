3df75dc46c259c6fb2eaec198d66103db0640d33
Saunak Nandi <saunaknandi021000@gmail.com>
2024-01-06 15:40:18 +0530

    changed

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
@@ -30,7 +31,7 @@ export async function POST(req: Request) {
   const payload = await req.json()
   const body = JSON.stringify(payload);
  
-  // Create a new Svix instance with your secret.
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
   let evt: WebhookEvent
@@ -49,52 +50,53 @@ export async function POST(req: Request) {
     })
   }
  
-  // Get the ID and type
-//   const { id } = evt.data;
   const eventType = evt.type;
- 
+
   console.log({eventType})
-    if(eventType==='user.created')
-    {
-        const {id,email_addresses,image_url,username,first_name,last_name}=evt.data
-        
-        // call a server action to Create a new user in your database
-        const mongoUser=await createUser({
-            clerkId:id,
-            name:`${first_name}${last_name? ` ${last_name}`:''}`,
-            username:username!, // username not null
-            email:email_addresses[0].email_address,
-            picture:image_url
-        })
+ 
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
 
-        return NextResponse.json({message:'OK',user:mongoUser})
-    }
-    else if(eventType==='user.updated')
-    {
-        const {id,email_addresses,image_url,username,first_name,last_name}=evt.data
-        
-        // call a server action to Create a new user in your database
-        const mongoUser=await updateUser({
-            clerkId:id,
-            updateData:{
-                name:`${first_name}${last_name? ` ${last_name}`:''}`,
-                username:username!, // username not null
-                email:email_addresses[0].email_address,
-                picture:image_url
-            },
-            path:`/profile/${id}`  // path, let Nextjs know that we have changed something
-        })
+    // Create a new user in your database
+    const mongoUser = await createUser({
+      clerkId: id,
+      name: `${first_name}${last_name ? ` ${last_name}` : ''}`,
+      username: username!,
+      email: email_addresses[0].email_address,
+      picture: image_url,
+    })
 
-        return NextResponse.json({message:'OK',user:mongoUser})
-    }
-    if(eventType==='user.deleted')
-    {
-        const {id}=evt.data
+    return NextResponse.json({ message: 'OK', user: mongoUser})
+  }
+  
+  if(eventType === 'user.updated') {
+    const { id, email_addresses, image_url, username, first_name, last_name } = evt.data;
 
-        const deletedUser=await deleteUser({
-            clerkId:id!
-        })
-    }
-  return new Response('', { status: 200 })
+    // Create a new user in your database
+    const mongoUser = await updateUser({
+      clerkId: id,
+      updateData: {
+        name: `${first_name}${last_name ? ` ${last_name}` : ''}`,
+        username: username!,
+        email: email_addresses[0].email_address,
+        picture: image_url,
+      },
+      path: `/profile/${id}`
+    })
+
+    return NextResponse.json({ message: 'OK', user: mongoUser})
+  }
+
+  if(eventType === 'user.deleted') {
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser({
+      clerkId: id!,
+    })
+
+    return NextResponse.json({ message: 'OK', user: deletedUser})
+  }
+ 
+  return new Response('', { status: 201 })
 }
  
